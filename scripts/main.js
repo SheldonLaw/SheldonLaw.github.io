e46945fac19f768c41a34c6d67695e447fb1094b
Sheldon Law <canch@outlook.com>
2017-03-06 20:20:47 +0800

    init 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,48 @@
 // register
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/scripts/service-worker.js')
+    navigator.serviceWorker.register('/scripts/service-worker.js').then(initialiseState);
 } else {
     alert("Your browser does not support service-worker.");
+}
+
+// Once the service worker is registered set the initial state
+function initialiseState() {
+    // Are Notifications supported in the service worker?
+    if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
+        alert('Notifications aren\'t supported.');
+        return;
+    }
+
+    // Check the current Notification permission.
+    // If its denied, it's a permanent block until the
+    // user changes the permission
+    if (Notification.permission === 'denied') {
+        alert('The user has blocked notifications.');
+        return;
+    }
+
+    // Check if push messaging is supported
+    if (!('PushManager' in window)) {
+        alert('Push messaging isn\'t supported.');
+        return;
+    }
+
+    // We need the service worker registration to check for a subscription
+    navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+        // Do we already have a push message subscription?
+        serviceWorkerRegistration.pushManager.getSubscription()
+            .then(function(subscription) {
+                if (!subscription) {
+                    // We aren’t subscribed to push, so set UI
+                    // to allow the user to enable push
+                    return;
+                }
+            })
+            .catch(function(err) {
+                alert(
+                    'Error during getSubscription()', err);
+            });
+    });
 }
 
 
