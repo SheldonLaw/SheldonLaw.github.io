f2a509cf1ea165e75e6e963cd7a35d8e83593446
Sheldon Law <canch@outlook.com>
2017-03-06 21:06:49 +0800

    add demo btn

--- scripts/main.js
+++ scripts/main.js
@@ -1,79 +1,101 @@
 // register
+var pushEnabled = false;
+var pushBtn = document.querySelector('#push-btn');
+var sendBtn = document.querySelector('#send-btn');
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/scripts/service-worker.js').then(initialiseState);
+    navigator.serviceWorker.register('./service-worker.js').then(initialiseState);
+    pushBtn.addEventListener('click', function() {
+        if (!pushEnabled) {
+            //注册
+            subscribe();
+        } else {
+            unSubscribe();
+        }
+    });
+    sendBtn.addEventListener('click', function() {
+        navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+            setTimeout(function() {
+                var title = 'Yay a message.';
+                var body = 'We have received a push message.';
+                var icon = '/images/icon-192x192.png';
+                var tag = 'simple-push-demo-notification-tag';
+                serviceWorkerRegistration.showNotification(title, {
+                    body: body,
+                    icon: icon,
+                    tag: tag,
+                    data: "123"
+                })
+            }, 3000);
+        });
+    });
 } else {
     alert("Your browser does not support service-worker.");
 }
 
-// Once the service worker is registered set the initial state
 function initialiseState() {
-    // Are Notifications supported in the service worker?
     if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
         alert('Notifications aren\'t supported.');
         return;
     }
 
-    // Check the current Notification permission.
-    // If its denied, it's a permanent block until the
-    // user changes the permission
     if (Notification.permission === 'denied') {
         alert('The user has blocked notifications.');
         return;
     }
 
-    // Check if push messaging is supported
     if (!('PushManager' in window)) {
         alert('Push messaging isn\'t supported.');
         return;
     }
 
-    // We need the service worker registration to check for a subscription
     navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-        // Do we already have a push message subscription?
         serviceWorkerRegistration.pushManager.getSubscription()
             .then(function(subscription) {
                 if (!subscription) {
-                    // We aren’t subscribed to push, so set UI
-                    // to allow the user to enable push
+                    pushBtn.textContent = "开启通知";
+                    pushBtn.disabled = false;
                     return;
                 }
             })
             .catch(function(err) {
-                alert(
-                    'Error during getSubscription()', err);
+                alert('Error during getSubscription()', err);
             });
     });
 }
 
 
 function subscribe() {
-
-    // We need the service worker registration to check for a subscription
     navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-        // var title = 'Yay a message.';
-        // var body = 'We have received a push message.';
-        // var icon = '/images/icon-192x192.png';
-        // var tag = 'simple-push-demo-notification-tag';
-        // serviceWorkerRegistration.showNotification(title, {
-        //     body: body,
-        //     icon: icon,
-        //     tag: tag,
-        //     data: "123"
-        // })
         serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly: true })
             .then(function(subscription) {
-                return subscription;
-                // return sendSubscriptionToServer(subscription);
+                //测试通知
+                var title = 'Yay a message.';
+                var body = 'We have received a push message.';
+                var icon = '/images/icon-192x192.png';
+                var tag = 'simple-push-demo-notification-tag';
+                serviceWorkerRegistration.showNotification(title, {
+                    body: body,
+                    icon: icon,
+                    tag: tag,
+                    data: "123"
+                })
+                //订阅通知
+                return sendSubscriptionToServer(subscription);
             })
             .catch(function(e) {
                 if (Notification.permission === 'denied') {
-                    alert(
-                        'Permission for Notifications was denied'
-                    );
+                    alert('Permission for Notifications was denied');
                 } else {
-                    alert(
-                        'Unable to subscribe to push.', e);
+                    alert('Unable to subscribe to push.', e);
                 }
             }); 
     });
+}
+
+function unSubscribe() {
+
+}
+
+function sendSubscriptionToServer(subscription) {
+
 }
