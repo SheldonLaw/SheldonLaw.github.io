f3106e9408911516acd9e7eea203f047bf3e0daf
Sheldon Law <canch@outlook.com>
2017-03-13 17:20:32 +0800

    add .eslintrc.json for js format

--- push-service/main.js
+++ push-service/main.js
@@ -3,46 +3,133 @@ var pushEnabled = false;
 var pushBtn = document.querySelector('#push-btn');
 var sendBtn = document.querySelector('#send-btn');
 
+function sendSubscriptionToServer (subscription) {
+    if (!subscription) {
+        pushBtn.textContent = '开启推送';
+        pushEnabled = false;
+    } else {
+        pushBtn.textContent = '关闭推送';
+        pushBtn.classList.add('on');
+        pushEnabled = true;
+    }
+    // 向服务器订阅
+}
+
+function subscribe () {
+    navigator.serviceWorker.ready.then(function (serviceWorkerRegistration) {
+        serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly: true })
+            .then(function (subscription) {
+                // 订阅通知
+                return sendSubscriptionToServer(subscription);
+            })
+            .catch(function (e) {
+                if (Notification.permission === 'denied') {
+                    alert('没有获取到权限');
+                } else {
+                    alert('发生错误', e);
+                }
+            });
+    });
+}
+
+function unSubscribe () {
+    navigator.serviceWorker.ready
+    .then(function (serviceWorkerRegistration) {
+        serviceWorkerRegistration.pushManager.getSubscription()
+        .then(function (pushSubscription) {
+            if (!pushSubscription) {
+                return;
+            }
+            pushSubscription.unsubscribe()
+            .then(function (successful) {
+                pushBtn.textContent = '开启推送';
+                pushBtn.classList.remove('on');
+                pushEnabled = false;
+            })
+            .catch(function (e) {
+                alert('Unsubscription error: ', e);
+            });
+        })
+        .catch(function (e) {
+            alert('Error thrown while unsubscribing from push messaging.', e);
+        });
+    });
+}
+
+function initialiseState () {
+    if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
+        alert('当前浏览器不支持通知。');
+        return;
+    }
+
+    if (Notification.permission === 'denied') {
+        alert('用户已屏蔽通知。');
+        return;
+    }
+
+    if (!('PushManager' in window)) {
+        alert('当前浏览器不支持推送。');
+        return;
+    }
+
+    navigator.serviceWorker.ready.then(function (serviceWorkerRegistration) {
+        serviceWorkerRegistration.pushManager.getSubscription()
+            .then(function (subscription) {
+                if (!subscription) {
+                    pushBtn.textContent = '开启推送';
+                    pushEnabled = false;
+                } else {
+                    pushBtn.textContent = '关闭推送';
+                    pushBtn.classList.add('on');
+                    pushEnabled = true;
+                }
+            })
+            .catch(function (err) {
+                alert('发生错误', err);
+            });
+    });
+}
+
 // load from localStorage
 var title, content, link;
-if(localStorage) {
+if (localStorage) {
     title = localStorage.title;
     content = localStorage.content;
     link = localStorage.link;
-    if(title) pushContentForm.title.value = title;
-    if(content) pushContentForm.content.value = content;
-    if(link) pushContentForm.link.value = link;
+    if (title) { pushContentForm.title.value = title; }
+    if (content) { pushContentForm.content.value = content; }
+    if (link) { pushContentForm.link.value = link; }
 }
 
-if('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./service-worker.js').then(initialiseState);
-    pushBtn.addEventListener('click', function() {
-        if(!pushEnabled) {
-            //注册
+    pushBtn.addEventListener('click', function () {
+        if (!pushEnabled) {
+            // 注册
             subscribe();
         } else {
             unSubscribe();
         }
     });
-    sendBtn.addEventListener('click', function() {
-        if(!pushEnabled) {
-            alert("请先开启推送。");
+    sendBtn.addEventListener('click', function () {
+        if (!pushEnabled) {
+            alert('请先开启推送。');
             return;
         }
 
         title = pushContentForm.title.value;
         content = pushContentForm.content.value;
         link = pushContentForm.link.value;
-        
-        //save into localStorage
-        if(localStorage) {
+
+        // save into localStorage
+        if (localStorage) {
             localStorage.title = title;
             localStorage.content = content;
             localStorage.link = link;
         }
 
-        navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-            setTimeout(function() {
+        navigator.serviceWorker.ready.then(function (serviceWorkerRegistration) {
+            setTimeout(function () {
                 title = title || '推送标题';
                 content = content || '推送内容';
                 // just for fun
@@ -56,98 +143,10 @@ if('serviceWorker' in navigator) {
                     // image: image,
                     tag: tag,
                     data: link
-                })
+                });
             }, 3000);
         });
     });
 } else {
-    alert("当前浏览器不支持 service-worker.");
-}
-
-function initialiseState() {
-    if(!('showNotification' in ServiceWorkerRegistration.prototype)) {
-        alert('当前浏览器不支持通知。');
-        return;
-    }
-
-    if(Notification.permission === 'denied') {
-        alert('用户已屏蔽通知。');
-        return;
-    }
-
-    if(!('PushManager' in window)) {
-        alert('当前浏览器不支持推送。');
-        return;
-    }
-
-    navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-        serviceWorkerRegistration.pushManager.getSubscription()
-            .then(function(subscription) {
-                if(!subscription) {
-                    pushBtn.textContent = "开启推送";
-                    pushEnabled = false;
-                } else {
-                    pushBtn.textContent = "关闭推送";
-                    pushBtn.classList.add('on'); 
-                    pushEnabled = true;
-                }
-            })
-            .catch(function(err) {
-                alert('发生错误', err);
-            });
-    });
-}
-
-
-function subscribe() {
-    navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-        serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly: true })
-            .then(function(subscription) {
-                //订阅通知
-                return sendSubscriptionToServer(subscription);
-            })
-            .catch(function(e) {
-                if(Notification.permission === 'denied') {
-                    alert('没有获取到权限');
-                } else {
-                    alert('发生错误', e);
-                }
-            }); 
-    });
-}
-
-function unSubscribe() {
-    navigator.serviceWorker.ready
-    .then(function(serviceWorkerRegistration) {
-        serviceWorkerRegistration.pushManager.getSubscription()
-        .then(function(pushSubscription) {
-            if(!pushSubscription) {
-                return;
-            }
-            pushSubscription.unsubscribe()
-            .then(function(successful) {
-                pushBtn.textContent = '开启推送';
-                pushBtn.classList.remove('on');
-                pushEnabled = false;
-            })
-            .catch(function(e) {
-                alert('Unsubscription error: ', e);
-            });
-        })
-        .catch(function(e) {
-            alert('Error thrown while unsubscribing from push messaging.', e);
-        });
-    });
-}
-
-function sendSubscriptionToServer(subscription) {
-    if(!subscription) {
-        pushBtn.textContent = "开启推送";
-        pushEnabled = false;
-    } else {
-        pushBtn.textContent = "关闭推送";
-        pushBtn.classList.add('on'); 
-        pushEnabled = true;
-    }
-    //向服务器订阅
+    alert('当前浏览器不支持 service-worker.');
 }
