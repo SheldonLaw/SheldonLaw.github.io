c04d1daa1b4d8b5afc8a6bd1599ddd15c2fae3a3
Sheldon Law <canch@outlook.com>
2017-03-30 17:42:06 +0800

    fixed code style

--- logging/main.js
+++ logging/main.js
@@ -1,47 +1,46 @@
 (function() {
-	'use strict';
+  'use strict';
+  // situation one:
+  function getNum(num) {
+    if (num > 5) {
+      return num;
+    }
+    return null;
+  }
+  var num = getNum(10);
+  Logging.check(num);
 
-	// situation one:
-	function getNum(num) {
-		if (num > 5) {
-			return num;
-		}
-		return null;
-	}
-	var num = getNum(10);
-	Logging.check(num);
+  setTimeout(function() {
+    num = getNum(3);
+    Logging.check(num);		// will break here
+  }, 0);
+  setTimeout(function() {
+    num = getNum(10);	// num = 10
+    Logging.check(num, 9);		// will break here
+  }, 0);
 
-	setTimeout(function() {
-		num = getNum(3);
-		Logging.check(num);		// will break here
-	}, 0);
-	setTimeout(function() {
-		num = getNum(10);	// num = 10
-		Logging.check(num, 9);		// will break here
-	}, 0);
-
-	// situation two:
-	function simpleSwitch(num) {
-		switch (num) {
-			case 1:
-			case 2:
-				break;
-			default:
-				Logging.unReach();
-		}
-	}
-	simpleSwitch(1);
-	simpleSwitch(2);
-	setTimeout(function() {
-		simpleSwitch(3);	// will break here
+  // situation two:
+  function simpleSwitch(num) {
+    switch (num) {
+      case 1:
+      case 2:
+        break;
+      default:
+        Logging.unReach();
+    }
+  }
+  simpleSwitch(1);
+  simpleSwitch(2);
+  setTimeout(function() {
+    simpleSwitch(3);	// will break here
 	}, 0);
 
 	// situation three:
 	setTimeout(function() {
-		var a = 'hello Logging';
-		var index = a.indexOf('Logging');
-		Logging.checkUE(index, -1);
-		index = a.indexOf('Logging.js');
-		Logging.checkUE(index, -1);	// will break here
-	}, 0);
+    var a = 'hello Logging';
+    var index = a.indexOf('Logging');
+    Logging.checkUE(index, -1);
+    index = a.indexOf('Logging.js');
+    Logging.checkUE(index, -1);	// will break here
+  }, 0);
 })();
